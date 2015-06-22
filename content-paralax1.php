87d59ffb673bb2ab7f95b33dfb28b327ad7a45df
Jarod Means <jsmeans2@gmail.com>
2015-06-22 16:31:45 -0500

    Adding Skrollr to project

---
+++ content-paralax1.php
@@ -0,0 +1,11 @@
+
+<div class="parallax">
+  <div class="parallax__group">
+    <div class="parallax__layer parallax__layer--back">
+      <img src="wp-content/themes/courtroom/img/lens.jpg">
+    </div>
+    <div class="parallax__layer parallax__layer--base">
+      ...
+    </div>
+  </div>
+</div>
